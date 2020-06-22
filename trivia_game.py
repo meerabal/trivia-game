e76925db691c89d244c1069da51114a6430579b9
meerabal <meera.balsara@gmail.com>
2020-06-22 00:07:28 -0400

    Added ask method, score tracking pending

--- trivia_game.py
+++ trivia_game.py
@@ -2,6 +2,7 @@ import urllib.request
 import sys
 import re
 import json
+import random
 
 # class holds all the attributes for an object of type Question
 class Question:
@@ -40,9 +41,8 @@ def obj_create(content):
     ques_dict = json.loads(content)["results"]      # could use eval(str) but eval is very powerful and should not be used if input is not completely trusted
     ques_list = []
     for q in ques_dict:
-        print(q)
         ques_list.append(Question(q["category"], q["type"], q["difficulty"], clean(q["question"]), clean(q["correct_answer"]), clean(q["incorrect_answers"])))
-        return ques_list
+    return ques_list
     '''# testing
     for q in ques_list:
         print(q.category, q.ques_type, q.difficulty, q.ques, q.correct_ans, str(q.incorrect_ans))'''
@@ -51,9 +51,9 @@ def obj_create(content):
 def init():
     return_str = ""
 
-    print("Trivia game")
-    print("The game will ask 10 questions from different categories and keep a track of your score.")
-    difficulty = input("Choose difficulty level: Easy (1) or Medium (2) or Hard (3) or Mixed (4)? ")
+    print("\n==================== Trivia game ====================")
+    print("\nThe game will ask 10 questions from different categories and keep a track of your score.")
+    difficulty = input("\nChoose difficulty level: Easy (1) or Medium (2) or Hard (3) or Mixed (4)? ")
     while True:
         if not(difficulty.isnumeric and 0 < int(difficulty) < 5):
             difficulty = input("Invalid input. Please enter \n1 for easy \n2 for medium \n3 for hard \n4 for mixed \n> ")
@@ -64,8 +64,9 @@ def init():
                 return_str += "&difficulty=medium"
             elif int(difficulty) == 3:
                 return_str += "&difficulty=hard"
+            break
     
-    ques_type = input("Choose type of questions: Multiple choice (1) or True / False (2) or Mixed (3)? ")
+    ques_type = input("\nChoose type of questions: Multiple choice (1) or True / False (2) or Mixed (3)? ")
     while True:
         if not(ques_type.isnumeric and 0 < int(ques_type) < 4):
             ques_type = input("Invalid input. Please enter \n1 for multiple choice \n2 for true / false \n3 for mixed \n> ")
@@ -74,13 +75,35 @@ def init():
                 return_str += "&type=multiple"
             elif int(ques_type) == 2:
                 return_str += "&type=boolean"
+            break
     
     return return_str
+
+def ask(ques_list):
+    for q in ques_list:
+        print()
+        print(q.ques)
+        if q.ques_type == "multiple":
+            shuffled_list = [q.correct_ans] + q.incorrect_ans
+            random.shuffle(shuffled_list)
+
+            for a in shuffled_list:
+                print("-", a)
+        if q.ques_type == "boolean":
+            print("True / False?")
+        user_ans = input("Your answer: ")
+        q.user_ans = user_ans
+        if q.correct_ans.lower() == user_ans.lower():
+            print("Your answer is correct!")
+        else:
+            print("Your answer is incorrect.")
+            print("The correct answer is:", q.correct_ans)
 
 # main method
 def main():
     ques_type = init()
     content = readurl("https://opentdb.com/api.php?amount=10" + ques_type)
     ques_list = obj_create(content)
+    ask(ques_list)
 
 main()
