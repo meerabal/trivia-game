f23060c6cba5d2e021d454970231437f824576f9
meerabal <meera.balsara@gmail.com>
2020-06-21 23:28:50 -0400

    Allows user to set question type and difficulty

--- trivia_game.py
+++ trivia_game.py
@@ -42,31 +42,45 @@ def obj_create(content):
     for q in ques_dict:
         print(q)
         ques_list.append(Question(q["category"], q["type"], q["difficulty"], clean(q["question"]), clean(q["correct_answer"]), clean(q["incorrect_answers"])))
-    # testing
+        return ques_list
+    '''# testing
     for q in ques_list:
-        print(q.category, q.ques_type, q.difficulty, q.ques, q.correct_ans, str(q.incorrect_ans))
+        print(q.category, q.ques_type, q.difficulty, q.ques, q.correct_ans, str(q.incorrect_ans))'''
 
+# sets type of question to be asked and difficulty
 def init():
+    return_str = ""
+
     print("Trivia game")
     print("The game will ask 10 questions from different categories and keep a track of your score.")
+    difficulty = input("Choose difficulty level: Easy (1) or Medium (2) or Hard (3) or Mixed (4)? ")
+    while True:
+        if not(difficulty.isnumeric and 0 < int(difficulty) < 5):
+            difficulty = input("Invalid input. Please enter \n1 for easy \n2 for medium \n3 for hard \n4 for mixed \n> ")
+        else:
+            if int(difficulty) == 1:
+                return_str += "&difficulty=easy"
+            elif int(difficulty) == 2:
+                return_str += "&difficulty=medium"
+            elif int(difficulty) == 3:
+                return_str += "&difficulty=hard"
     
-    ques_type = input("Choose type of questions: Multiple choice (1) or True / False (2) or Mixed (3)?")
+    ques_type = input("Choose type of questions: Multiple choice (1) or True / False (2) or Mixed (3)? ")
     while True:
         if not(ques_type.isnumeric and 0 < int(ques_type) < 4):
             ques_type = input("Invalid input. Please enter \n1 for multiple choice \n2 for true / false \n3 for mixed \n> ")
         else:
             if int(ques_type) == 1:
-                return "&type=multiple"
+                return_str += "&type=multiple"
             elif int(ques_type) == 2:
-                return "&type=boolean"
-            else:
-                return ""
-
+                return_str += "&type=boolean"
+    
+    return return_str
 
 # main method
 def main():
     ques_type = init()
     content = readurl("https://opentdb.com/api.php?amount=10" + ques_type)
-    obj_create(content)
+    ques_list = obj_create(content)
 
 main()
